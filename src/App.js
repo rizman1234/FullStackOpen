20a7663c2ebc9693e31b4bfbde541698d2f7839e
Rizwan Halani <rizwan.halani@gmail.com>
2022-12-20 21:27:32 -0600

    Working with useState hook and re-rendering

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-const Hello = (props) => {
-  return (
-    <div>
-      <p>Hello {props.name}, you are {props.age} years old</p>
-    </div>
-  )
-}
+import {useState} from 'react'
+
 
 const App = () => {
+const [counter, setCounter] = useState(0)
 
-  const name = 'Peter'
-  const age = 10
+const increaseByOne = () => setCounter(counter + 1)
 
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name = 'George' age = {26 + 10} />
-      <Hello name = {name} age = {age} />
-    </div>
-  ) 
-  
+const setToZero = () => setCounter(0)
+
+// const handleClick = () => {
+//   console.log('clicked')
+// }
+
+
+// setTimeout (
+//   () => setCounter(counter + 1),
+//   1000
+// )
+
+return (
+  <div>
+  <div>{counter}</div>
+  <button onClick={increaseByOne}>
+    plus
+  </button>
+  <button onClick = {setToZero}>
+    zero
+    </button>
+  </div>
+) 
 }
 
 export default App
